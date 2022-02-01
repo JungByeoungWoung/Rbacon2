d8e4962c9d02fc82a62d299d414741637ca2cd50
JungByeoungWoung <jbw03456@naver.com>
2022-02-01 13:29:54 +0900

    Initial commit

---
+++ src/main/java/com/Rbacon2/Rbacon2Application.java
@@ -0,0 +1,13 @@
+package com.Rbacon2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rbacon2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rbacon2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Rbacon2/Rbacon2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rbacon2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rbacon2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
