6d1c63630932ec7d27d7883ea82c54537a6eae32
JungByeoungWoung <jbw03456@naver.com>
2022-02-03 23:16:27 +0900

    홈 화면에서 회원 가입 화면으로 경로 설정 수정 완료

--- src/main/java/com/Rbacon2/domain/web/basic/BasicManagerController.java
+++ src/main/java/com/Rbacon2/domain/web/basic/BasicManagerController.java
@@ -13,26 +13,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller
-@RequestMapping("/basic/user/add")
+@RequestMapping("/basic/addForm")
 @RequiredArgsConstructor
 public class BasicManagerController {
     private Repository memoryRepository = new ManagerMemoryRepository();
 
     @GetMapping
-    public String save(@RequestParam String testName,
-                       @RequestParam String testId,
-                       @RequestParam String testPwd,
-                       @RequestParam String testEmail,
-                       @RequestParam String testPhone,
-                       Model model) {
-        ManagerUser managerUser = new ManagerUser();
-        managerUser.setTestName(managerUser.getTestName());
-        managerUser.setTestId(managerUser.getTestId());
-        managerUser.setTestPwd(managerUser.getTestPwd());
-        managerUser.setTestEmail(managerUser.getTestEmail());
-        managerUser.setTestPhone(managerUser.getTestPhone());
-        memoryRepository.save(managerUser);
-        log.info("debug log={}",managerUser.toString());
+    public String saveForm() {
         return "basic/addForm";
     }
+//    public String save(@RequestParam String testName,
+//                       @RequestParam String testId,
+//                       @RequestParam String testPwd,
+//                       @RequestParam String testEmail,
+//                       @RequestParam String testPhone,
+//                       Model model) {
+//        ManagerUser managerUser = new ManagerUser();
+//        managerUser.setTestName(managerUser.getTestName());
+//        managerUser.setTestId(managerUser.getTestId());
+//        managerUser.setTestPwd(managerUser.getTestPwd());
+//        managerUser.setTestEmail(managerUser.getTestEmail());
+//        managerUser.setTestPhone(managerUser.getTestPhone());
+//        memoryRepository.save(managerUser);
+//        log.info("debug log={}",managerUser.toString());
+//        return "basic/addForm";
+//    }
 }
